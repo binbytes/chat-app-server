2da4b5d8254e96acf1ad33654da18d6505304e46
Nikunj Kanetiya <kanetiya@gmail.com>
2017-10-12 10:52:12 +0530

    Handle user online status

--- socketEvents.js
+++ socketEvents.js
@@ -1,10 +1,31 @@
 export default function (io) {
+
+  // Currently lets just keep records of user id & their socket id
+  let userSockets = {}
+
   // Set socket.io listeners.
   io.on('connection', (socket) => {
     console.log('a socket client connected')
 
     socket.on('disconnect', () => {
-      console.log('socket client disconnected')
+      let userId = userSockets[socket.id]
+      delete userSockets[socket.id]
+
+      console.log('disconnect', userId, userSockets)
+
+      if (userId) {
+        io.sockets.emit('user-offline', userId)
+      }
+    })
+
+    socket.on('online-ping', (userId) => {
+      if (userId) {
+        userSockets[socket.id] = userId
+        socket.emit('welcome', { 'test': 'welcome' })
+        socket.broadcast.emit('user-online', userId)
+
+        console.log('asdas', Object.values(userSockets))
+      }
     })
 
     socket.on('enter-conversation', (conversation) => {
